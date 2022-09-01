2d40d3c711fc56afb1ee7aaa1ae8dbd8584d2caa
Katie <bonkka@clarkson.edu>
2022-09-01 12:31:44 -0400

    Number 1!

---
+++ src/HelloMain.java
@@ -0,0 +1,9 @@
+//Katie Bonk 8/30
+import java.util.*;
+
+public class HelloMain {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
+//classes start with capital, variable and functions lowercase start, constants all upper with _
